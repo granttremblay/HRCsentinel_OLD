d61b657fb19c979e86b5ddc69e618ca21b21e7b2
Grant Tremblay <granttremblay@gmail.com>
2017-07-26 16:26:16 -0400

    Yeah, deal with it.

---
+++ environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+SKA=/home/grant/Engineering/sot/ska;
+export SKA;
+
+#SKA_BIN=/home/grant/Engineering/sot/ska/bin;
+#export SKA_BIN;
+
+
+#SKA_DATA=/home/grant/Engineering/sot/ska/data;
+#export SKA_DATA;
+
