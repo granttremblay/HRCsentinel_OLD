0dcb2765d99a90130321a1412516007248b8c98e
Grant Tremblay <granttremblay@gmail.com>
2017-07-26 16:28:02 -0400

    Yeah, deal with it.

--- environment.sh
+++ environment.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-
 SKA=/home/grant/Engineering/sot/ska;
 export SKA;
 
-#SKA_BIN=/home/grant/Engineering/sot/ska/bin;
-#export SKA_BIN;
-
-
-#SKA_DATA=/home/grant/Engineering/sot/ska/data;
-#export SKA_DATA;
+echo "Ska environment defined"
 
