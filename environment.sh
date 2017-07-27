796a042fe27cea168e33fca7e3650c992a74f993
Grant Tremblay <granttremblay@gmail.com>
2017-07-27 16:18:35 -0400

    Yeah, deal with it.

--- environment.sh
+++ environment.sh
@@ -3,5 +3,10 @@
 SKA=/home/grant/Engineering/sot/ska;
 export SKA;
 
-echo "Ska environment defined"
+echo "Ska environment defined: $SKA"
 
+py2
+echo "Ensuring you're using Python 2.X:"
+which python
+
+echo "Done"
